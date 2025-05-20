87cdf4e021af219a34c0bd231eda3772e8811327
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-05-20 16:51:50 -0300

    Update app.constants.ts

--- frontend/src/app/core/constants/app.constants.ts
+++ frontend/src/app/core/constants/app.constants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
 
-  //static readonly BASE_URL = 'https://revisao-carro.onrender.com';
-  static readonly BASE_URL = 'http://localhost:8080'
+  static readonly BASE_URL = 'https://revisao-carro.onrender.com';
+  //static readonly BASE_URL = 'http://localhost:8080'
 
   static readonly MARCA_URL = `${AppConstants.BASE_URL}/marca`;
   static readonly MODELO_URL = `${AppConstants.BASE_URL}/modelo`;
