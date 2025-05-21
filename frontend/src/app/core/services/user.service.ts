7af914664241e92d2dfd3d49d9a79db68a450fb1
Ryan Carvalho Bernardo <ryan.bernardo@tre-ba.gov.br>
2025-05-21 11:09:33 -0300

    feat: patch function

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from "@angular/core";
-import { BaseService } from "./base.service";
-import { HttpClient } from "@angular/common/http";
-import { AppConstants } from "../constants/app.constants";
+import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+import { HttpClient } from '@angular/common/http';
+import { AppConstants } from '../constants/app.constants';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
+export class UserService extends BaseService {
+  constructor(http: HttpClient) {
+    super(http);
+    this.path = AppConstants.USER_URL;
+  }
 
-export class UserService extends BaseService{
-
-    constructor(http: HttpClient){
-        super(http);
-        this.path=AppConstants.USER_URL;
-    }
-
-
-
+  patchRole(role: string[], id: number): Observable<User> {
+    const url = `${AppConstants.USER_URL}/${id}`;
+    return this.http.patch<User>(this.path, { role });
+  }
 }
