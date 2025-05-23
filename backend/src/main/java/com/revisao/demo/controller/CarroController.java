ee181cb07e0112cdc12bd43a564550601d4042fb
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-05-23 08:47:27 -0300

    improve: car details

--- backend/src/main/java/com/revisao/demo/controller/CarroController.java
+++ backend/src/main/java/com/revisao/demo/controller/CarroController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,12 @@ public class CarroController {
 	@GetMapping("/{idCarro}/km/recente")
 	public Double getKmRecente(@PathVariable Integer idCarro) {
 		return carroService.getKmRecente(idCarro);
+	}
+	
+	@PutMapping("/{idCarro}")
+	public ResponseEntity<Void> putCarro(@PathVariable Integer idCarro, @RequestBody CarroDTO updateData){
+		carroService.update(idCarro, updateData);
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 	
 }
